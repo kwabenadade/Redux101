7783090cd4e6b8f45889156a4304cece965d2a8c
coby <coby@localhost.localdomain>
2023-09-25 18:09:39 -0400

    for the boys

---
+++ src/features/POST/postSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [{
+    id: "1",title:'Learning Redux Toolkit ', content:"i've heard good things"
+},{id: '2', title:'slices', content:"the more i say slice,the more i want pizza"}
+
+]
+
+const postsSlice = createSlice({
+    name:"posts",
+    initialState,
+    reducers:{
+        
+        postAdded(state, action){
+            state.push(action.payload);
+        }
+    }
+
+})
+
+export const selectAllPosts = (state) => state.posts;
+export const {postAdded} = postsSlice.actions
+
+
+export default postsSlice.reducer;
